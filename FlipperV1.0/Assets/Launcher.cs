d7d3e037e932f49285f45a8b7f56131e95dc10f2
unknown <usrlocal@GA211-FE09.ad.univ-angers.fr>
2017-11-21 15:26:15 +0100

    marlish la famille

---
+++ FlipperV1.0/Assets/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+    string inputButtonName = "Pull";
+    public GameObject ball;
+    public float Force = 5;
+    bool Play = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            if (Input.GetButtonDown(inputButtonName) && Play ) {
+            ball.GetComponent<Collider>().GetComponent<Rigidbody>().AddForce(Force * Vector3.left);
+
+            }
+        }
+}
